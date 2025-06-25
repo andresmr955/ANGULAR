32f6e67de6295e1dfb9f2227bc091277ac8151c1
Andres Marquez <andresmr955@gmail.com>
2025-06-25 10:56:08 -0400

    learning how to add, delete with control structures, click and change

--- todoapp/src/app/pages/controlstructures/controlstructures.ts
+++ todoapp/src/app/pages/controlstructures/controlstructures.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-controlstructures',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './controlstructures.html',
   styleUrl: './controlstructures.css'
 })
 export class Controlstructures {
+  tasks1 = signal(
+    [
+      'Instalar Angular', 
+      'Create proyect',
+      'Create component',
+      'create service'
+    ]
+  );
 
+  tasks = signal(
+    [
+      'Instalar Angular', 
+      'Create proyect',
+      'Create component',
+      'create service'
+    ]
+  );
+
+  changeHandler(event: Event){
+      const input = event.target as HTMLInputElement;
+      const newTask = input.value;
+      this.tasks.update((tasks) =>  [...tasks, newTask]);
+  }
+
+  deleteTask(index: number){
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 }
--- todoapp/src/app/pages/home/home.ts
+++ todoapp/src/app/pages/home/home.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  tasks = signal(
+    [
+      'Instalar Angular', 
+      'Create proyect',
+      'Create component',
+      'create service'
+    ]
+  );
 
+  
+  changeHandler(event: Event){
+      const input = event.target as HTMLInputElement;
+      const newTask = input.value;
+      this.tasks.update((tasks) =>  [...tasks, newTask]);
+  }
+  
+  deleteTask(index: number){
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index ));
+  }
 }

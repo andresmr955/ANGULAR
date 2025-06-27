b841d24fc6ad73c2f23a705d8beac705163e3c33
Andres Marquez <andresmr955@gmail.com>
2025-06-27 12:50:53 -0400

    learning apps web tasks

--- todoapp/src/app/pages/gettingreadyapp/gettingreadyapp.ts
+++ todoapp/src/app/pages/gettingreadyapp/gettingreadyapp.ts
@@ -48,5 +48,39 @@ export class Gettingreadyapp {
   }
   form: FormArray<FormControl<boolean>> = new FormArray
   (this.tasks().map(task => new FormControl(task.completed, { nonNullable: true})));
+
+    updateTaskEditingMode(index: number) {
+    if (this.tasks()[index].completed) return;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if(position === index){
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing: false
+        }
+      })
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if(position === index){
+          return {
+            ...task,
+            title: input.value,
+            editing: false,
+          }
+        }
+        return task
+      })
+    });
+  }
 }
   

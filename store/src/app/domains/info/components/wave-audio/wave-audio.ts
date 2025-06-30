15207b22d3a0349230fbd5dd8313f844e4790527
Andres Marquez <andresmr955@gmail.com>
2025-06-30 17:38:08 -0400

    creating the audio finished

--- store/src/app/domains/info/components/wave-audio/wave-audio.ts
+++ store/src/app/domains/info/components/wave-audio/wave-audio.ts
@@ -1,6 +1,5 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, signal } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
-import  waveSurfer  from 'wavesurfer.js'
 
 @Component({
   selector: 'app-wave-audio',
@@ -13,10 +12,20 @@ export class WaveAudio {
     @Input({required: true}) audioUrl!: string;
     @ViewChild('wave') container!: ElementRef;
 
+    private ws!: WaveSurfer;
+    isPlaying = signal(false);
+
     ngAfterViewInit(){
-      WaveSurfer.create({
+      this.ws = WaveSurfer.create({
         url: this.audioUrl,
         container: this.container.nativeElement
-      })    
+      });
+      this.ws.on('play',() =>  this.isPlaying.set(true));
+      this.ws.on('pause', () => this.isPlaying.set(false));
     }
-}
+
+    playingPause(){
+      this.ws.playPause()
+    }
+    
+  }
--- store/src/app/domains/info/pages/about/about.ts
+++ store/src/app/domains/info/pages/about/about.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Counter } from "../../../shared/components/counter/counter";
-
+import { WaveAudio } from "../../../info/components/wave-audio/wave-audio";
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, Counter],
+  imports: [CommonModule, Counter, WaveAudio],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
-  duration = signal(1000)
+  duration = signal(0)
   message = signal("Hola")
 
   changeDuration(event: Event) {

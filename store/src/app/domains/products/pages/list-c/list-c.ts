a4d8e1f301864741431d4208bb067974778eccab
Andres Marquez <andresmr955@gmail.com>
2025-07-02 14:59:10 -0400

    connecting my first API REST to angular

--- store/src/app/domains/products/pages/list-c/list-c.ts
+++ store/src/app/domains/products/pages/list-c/list-c.ts
@@ -4,7 +4,7 @@ import { ProductModel } from './../../../shared/models/product.model';
 import { ProductC } from './../../../products/components/product-c/product-c';
 import { Header } from "../../../shared/components/header-c/header-c";
 import { Cart } from './../../../shared/services/cart';
-
+import { Product } from './../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list-c',
@@ -17,25 +17,43 @@ export class ListC {
   productsc = signal<ProductModel[]>([]);
   // cart = signal<ProductModel[]>([]);
   private cartService = inject(Cart)
+  private productService = inject(Product)
+    
+
   cart = this.cartService.cart;
-  constructor(){
-    const initProducts: ProductModel[] = [
-      {
-        id: Date.now(),
-        title: 'pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r24',
-        creationAt: new Date().toISOString()     
-      },
-       {
-        id: Date.now(),
-        title: 'pro 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r23',
-        creationAt: new Date().toISOString()           
+
+  ngOnInit(){
+    this.productService.getProducts()
+    .subscribe({
+        next: (products) => {
+          this.productsc.set(products);
+        },
+        error: () => {
+
+        }
       }
-    ]
-    this.productsc.set(initProducts)
+    )
+  }
+
+
+  constructor(){
+    // const initProducts: ProductModel[] = [
+    //   {
+    //     id: Date.now(),
+    //     title: 'pro 1',
+    //     price: 100,
+    //     image: 'https://picsum.photos/640/640?r24',
+    //     creationAt: new Date().toISOString()     
+    //   },
+    //    {
+    //     id: Date.now(),
+    //     title: 'pro 1',
+    //     price: 100,
+    //     image: 'https://picsum.photos/640/640?r23',
+    //     creationAt: new Date().toISOString()           
+    //   }
+    // ]
+    // this.productsc.set(initProducts)
   }
   addToCart(product: ProductModel){
     //event: Event
---
+++ store/src/app/domains/shared/services/product.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { ProductModel } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Product {
+  private http = inject(HttpClient)
+  constructor() { }
+
+  getProducts(){
+    return this.http.get<ProductModel[]>('https://fakestoreapi.com/products')
+  }
+}
